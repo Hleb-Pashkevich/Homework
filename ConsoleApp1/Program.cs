3cfe0b797d378ba4bec78db9ecf94252f19c6abb
Hleb Pashkevich <hleb.pashkevich@gmail.com>
2023-12-18 21:19:35 +0300

    Lesson1 Completed

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Потребовать ввод от пользователя
+Console.Write("Enter your name: ");
+
+// Считать введенное имя с консоли
+string userName = Console.ReadLine();
+
+// Вывести приветствие на консоль
+Console.WriteLine($"Hello {userName}!");
+
+// Ждать, пока пользователь нажмет клавишу перед выходом
+Console.ReadLine();
